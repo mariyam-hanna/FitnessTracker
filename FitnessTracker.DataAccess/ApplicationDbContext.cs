b55753126307275b2eac2c0e4831892ba5c57270
mariyam-hanna <havvamariyam2001@gmail.com>
2025-09-18 19:47:17 +0530

    fitness

---
+++ FitnessTracker.DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,62 @@
+﻿using FitnessTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitnessTracker.DataAccess
+{
+    public class ApplicationDbContext :DbContext  
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+            new Category
+            {
+                Id = 1,DisplayOrder=1,MuscleGroup="Chest",
+                ImageUrl = "/images/chest.jpg"
+
+            }, new Category
+            {
+                Id = 2,
+                DisplayOrder = 2,
+                MuscleGroup = "biceps",
+                ImageUrl = "/images/biceps.jpg"
+
+            }, new Category
+            {
+                Id = 3,
+                DisplayOrder = 3,
+                MuscleGroup = "Tricep",
+                ImageUrl = "/images/tricep.jpg"
+            }, new Category
+            {
+                Id = 4,
+                DisplayOrder = 4,
+                MuscleGroup = "shoulder",
+                ImageUrl = "/images/shoulder.jpg"
+            }, new Category
+            {
+                Id = 5,
+                DisplayOrder = 5,
+                MuscleGroup = "legs",
+                ImageUrl = "/images/legs.jpg"
+            }, new Category
+            {
+                Id = 6,
+                DisplayOrder = 6,
+                MuscleGroup = "Back",
+                ImageUrl = "/images/back.jpg"
+            }, new Category
+            {
+                Id = 7,
+                DisplayOrder = 7,
+                MuscleGroup = "Neck",
+                ImageUrl = "/images/neck.jpg"
+            }
+            );
+        }
+    }
+}
---
+++ FitnessTracker.DataAccess/Migrations/20250821080930_addImageUrlToDb.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitnessTracker.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addImageUrlToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Workout",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Duration = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Workout", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Workout_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "/images/chest.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "/images/biceps.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "/images/tricep.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ImageUrl",
+                value: "/images/shoulder.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ImageUrl",
+                value: "/images/legs.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ImageUrl",
+                value: "/images/back.jpg");
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "DisplayOrder", "ImageUrl", "MuscleGroup" },
+                values: new object[] { 7, 7, "/images/neck.jpg", "Neck" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Workout_CategoryId",
+                table: "Workout",
+                column: "CategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Workout");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Categories");
+        }
+    }
+}
